777451590125764d6603c474788b8b4c2c5cc83b
James Kyburz <james.kyburz@gmail.com>
2017-06-11 14:11:20 +0200

    refactor ui routes

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -6,15 +6,33 @@ const Moment = require('moment')
 const client = require('level-eventstore').client({
   wsUrl: process.env.EVENTSTORE_URL || 'ws://guest:guest@eventstore:5000'
 })
-service('level-eventstore-ui', (router, ctx) => {
-  router.set('/api/logList', {
+
+const uiIgnored = [ '.DS_Store', '.git/', 'node_modules' ]
+const uiPath = path.join(__dirname, 'ui/build/')
+const uiFlags = { single: true, auth: !!process.env.SERVE_USER }
+
+process.env.ASSET_DIR = '/' + Math.random().toString(36).substr(2, 10)
+
+service('level-eventstore-ui', {
+  '@setup': (ctx, router) => {
+    ctx.use((req, res, next) => {
+      if (router.get(req.url).handler) return next()
+      if (req.url.match(/css$/i)) req.url = req.url.match(/\/css\/.*/i)[0]
+      if (req.url.match(/js$/i)) req.url = req.url.match(/\/static\/js.*/i)[0]
+      const cookieValue = encodeURIComponent(process.env.REACT_APP_BASE_URL)
+      const cookieName = 'LEVEL_EVENTSTORE_UI_REACT_APP_BASE_URL'
+      const cookie = `${cookieName}=${cookieValue}; Path=/;`
+      res.setHeader('Set-Cookie', cookie)
+      serve(req, res, uiFlags, uiPath, uiIgnored)
+    })
+  },
+  '/api/logList': {
     * get (req, res) {
       const list = yield (cb) => client.logList(cb)
       res.json(list)
     }
-  })
-
-  router.set('/api/logStream/:log/:since', {
+  },
+  '/api/logStream/:log/:since': {
     get (req, res, params) {
       params.log = params.log || ''
       params.since = +params.since || 0
@@ -33,9 +51,8 @@ service('level-eventstore-ui', (router, ctx) => {
         rows.push(data)
       })
     }
-  })
-
-  router.set('/api/streamById/:log/:id/:since', {
+  },
+  '/api/streamById/:log/:id/:since': {
     get (req, res, params) {
       params.log = params.log || ''
       params.id = params.id || ''
@@ -51,21 +68,5 @@ service('level-eventstore-ui', (router, ctx) => {
         rows.push(data)
       })
     }
-  })
-
-  const uiIgnored = [ '.DS_Store', '.git/', 'node_modules' ]
-  const uiPath = path.join(__dirname, 'ui/build/')
-  process.env.ASSET_DIR = '/' + Math.random().toString(36).substr(2, 10)
-  const uiFlags = { single: true }
-  if (process.env.SERVE_USER) uiFlags.auth = true
-  ctx.use((req, res, next) => {
-    if (router.get(req.url).handler) return next()
-    if (req.url.match(/css$/i)) req.url = req.url.match(/\/css\/.*/i)[0]
-    if (req.url.match(/js$/i)) req.url = req.url.match(/\/static\/js.*/i)[0]
-    const cookieValue = encodeURIComponent(process.env.REACT_APP_BASE_URL)
-    const cookieName = 'LEVEL_EVENTSTORE_UI_REACT_APP_BASE_URL'
-    const cookie = `${cookieName}=${cookieValue}; Path=/;`
-    res.setHeader('Set-Cookie', cookie)
-    serve(req, res, uiFlags, uiPath, uiIgnored)
-  })
+  }
 }).start(5000)
